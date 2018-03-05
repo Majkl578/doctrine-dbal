096c5d74197f084b460dad0d3f7b643ecfa58677
stlrnz <stlrnz@users.noreply.github.com>
2018-03-05 09:21:15 +0100

    added table listing test

---
+++ tests/Doctrine/Tests/DBAL/Schema/SQLServerSchemaManagerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Doctrine\Tests\DBAL\Schema;
+
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Platforms\SQLServerPlatform;
+use Doctrine\DBAL\Schema\SQLServerSchemaManager;
+
+/**
+ * @covers \Doctrine\DBAL\Schema\SQLServerSchemaManager
+ */
+final class SQLServerSchemaManagerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testListTableNames()
+    {
+        $driverMock = $this->createMock(Driver::class);
+        $platform   = $this->createMock(SQLServerPlatform::class);
+        $connection = $this
+            ->getMockBuilder(Connection::class)
+            ->setMethods(['fetchAll'])
+            ->setConstructorArgs([['platform' => $platform], $driverMock, new Configuration()])
+            ->getMock();
+
+        $connection->expects($this->once())->method('fetchAll')->will(
+            $this->returnValue(
+                [
+                    [
+                        'name'        => 'myTable',
+                        'schema_name' => 'mySchema',
+                    ],
+                    [
+                        'name'        => 'mySecondTable',
+                        'schema_name' => 'mySchema',
+                    ],
+                    [
+                        'name'        => 'myThirdTable',
+                        'schema_name' => 'dbo',
+                    ],
+                    [
+                        'name' => 'myFourthTable',
+                    ],
+                ]
+            )
+        );
+
+        $manager = new SQLServerSchemaManager($connection);
+
+        self::assertSame(
+            [
+                'mySchema.myTable',
+                'mySchema.mySecondTable',
+                'myThirdTable',
+                'myFourthTable',
+            ],
+            $manager->listTableNames()
+        );
+    }
+}
